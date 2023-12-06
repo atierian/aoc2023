bcb3dd4aeac6f14d05279236b502c8eff98d0fc8
Ian Saultz <52051793+atierian@users.noreply.github.com>
2023-12-06 10:01:52 -0500

    day06

---
+++ Sources/Day06.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+struct Day06: AdventDay {
+  var input: String
+
+  func part1() -> Int {
+    let lines = input.lines
+      .map {
+        $0.split(separator: " ")
+          .compactMap(Double.init)
+      }
+
+    return zip(lines[0], lines[1]).map(quadratic).product
+  }
+
+  func part2() -> Int {
+    let lines = input.lines
+      .map { [Double(String($0.filter(\.isNumber)))!] }
+
+    return zip(lines[0], lines[1]).map(quadratic).product
+  }
+
+  private func quadratic(a: Double, b: Double) -> Int {
+    let root = (a * a - 4 * b).squareRoot()
+    let x = [-root, root].map { ($0 - a) / 2 }
+    let result = Int(ceil(x[1] - 1) - floor(x[0] + 1)) + 1
+    return result
+  }
+}
---
+++ Tests/Day06.swift
@@ -0,0 +1,20 @@
+import XCTest
+@testable import AdventOfCode
+
+final class Day06Tests: XCTestCase {
+  let testInput = """
+    Time:      7  15   30
+    Distance:  9  40  200
+
+    """
+
+  func testPart1() throws {
+    let challenge = Day06(input: testInput)
+    XCTAssertEqual(challenge.part1(), 288)
+  }
+
+  func testPart2() throws {
+    let challenge = Day06(input: testInput)
+    XCTAssertEqual(challenge.part2(), 71503)
+  }
+}
